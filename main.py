36a351b3bb1ddf5eaa936d256b32c05707de07f2
Глеб Троц <gntrots@mts.ru>
2025-06-01 10:24:05 +0500

    Добавил систему упражнений через роутер и состояния

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-
+from bot.handlers.exercise import router as exercise_router
 from bot.handlers.start import start_command
 from data.db import init_db
 
@@ -15,6 +15,7 @@ load_dotenv()
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 user = os.getenv("USER_ID")
 dp = Dispatcher()
+dp.include_router(exercise_router)
 
 
 @dp.message(Command("start"))
